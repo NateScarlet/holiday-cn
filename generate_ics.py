885d2e58d5757d7acfa759744e3a433ab5502b31
NateScarlet <NateScarlet@Gmail.com>
2021-10-10 02:30:33 +0800

    fix: should not set encoding for binary file

--- generate_ics.py
+++ generate_ics.py
@@ -79,5 +79,5 @@ def generate_ics(data, filename):
             name = "上班(补" + name + ")"
         cal.add_component(_create_event(name, start, end))
 
-    with open(filename, "wb", encoding="utf8") as ics:
+    with open(filename, "wb") as ics:
         ics.write(cal.to_ical())
